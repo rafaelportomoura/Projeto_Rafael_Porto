d041effe010ad6f055ae7ea1ed07510bb3aae830
rafaelportomoura <rafaelportomoura@gmail.com>
2022-07-03 03:01:44 -0300

    Criando o Método MA3()

--- classA.cpp
+++ classA.cpp
@@ -11,9 +11,10 @@ public:
   int getA1() { return A1; }
   void setA1( int A1 ) { this->A1 = A1; }
   float getA2() { return A2; }
-  void setA2( int A2 ) { this->A2 = A2; }
+  void setA2( float A2 ) { this->A2 = A2; }
   void MA1() { cout << "MA1"; }
   void MA2() { cout << "MA2"; }
+  void MA3() { cout << "Alteração a classe A partir do clone"; }
 };
 
 int main() {
