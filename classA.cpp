1185e651c7420855221c181632197ec2c394303c
Rafael Moura <55251760+rafaelportomoura@users.noreply.github.com>
2022-07-03 02:24:07 -0300

    Add files via upload

---
+++ classA.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+private:
+  int A1;
+  float A2;
+public:
+  A() {}
+  int getA1() { return A1; }
+  void setA1( int A1 ) { this->A1 = A1; }
+  float getA2() { return A2; }
+  void setA2( int A2 ) { this->A2 = A2; }
+  void MA1() { cout << "MA1"; }
+  void MA2() { cout << "MA2"; }
+};
+
+int main() {
+
+  return 0;
+}
---
+++ classB.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class B {
+private:
+  int B1;
+  float B2;
+public:
+  B() {}
+  int getB1() { return B1; }
+  void setB1( int B1 ) { this->B1 = B1; }
+  float getB2() { return B2; }
+  void setB2( int B2 ) { this->B2 = B2; }
+  void MB1() { cout << "MB1"; }
+  void MB2() { cout << "MB2"; }
+};
+
+int main() {
+
+  return 0;
+}
