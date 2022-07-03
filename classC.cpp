88f0f3156d60b28bd482799f37d4f3f7301aeefd
rafaelportomoura <rafaelportomoura@gmail.com>
2022-07-03 03:00:54 -0300

    Criação da Classe C

---
+++ classC.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class C {
+  private:
+  int C1;
+  string C2;
+  public:
+  C() {}
+  int getC1() { return C1; }
+  void setC1( int C1 ) { this->C1 = C1; }
+  string getC2() { return C2; }
+  void setC2( string C2 ) { this->C2 = C2; }
+  void MC1() { cout << "MC1"; }
+  void MC2() { cout << "MC2"; }
+};
+
+int main() {
+
+  return 0;
+}
